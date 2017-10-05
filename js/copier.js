b30c4d6b088db7a7b37ee9ac0749a568f1c79805
Jason <jason.p.mcelhenney@gmail.com>
2017-10-04 21:49:52 -0500

    Version 0.1
    added functional copy icon
    added hover & click status bars in "code window"

--- js/copier.js
+++ js/copier.js
@@ -2,11 +2,50 @@
  * Created by jason on 10/3/17.
  */
 
+/*
+ * Replace all SVG images with inline SVG
+ */
+
 addCopyIcons();
 
 function addCopyIcons() {
+
+    var copyIconURL = chrome.extension.getURL("resources/font-awesome-copy.svg");
+
     $("pre code").each(function( index ) {
-        $(this).parent().append("<img src='/resources/copyicon.png' height='40' width='40'>");
+        $(this).parent().wrap("<div class='copy-wrapper'></div>");
+
+        var wrapper = $(this).parent().parent();
+
+        wrapper.append(
+            "<img class='svg copy-btn' src=' "+ copyIconURL +"' height='40' width='40'>"
+        );
+    });
+
+    $(".copy-btn").each(function( index ) {
+
+       $(this).hover(
+           function() {
+               $(this).parent().each(function() {
+                $(this).prepend("<span class='copy-popup'>Copy</span>");
+           });
+        },
+           function() {
+              $(this).parent().each(function() {
+                   $(this).children(":first.copy-popup, .copied-popup").each(function() {
+                       $(this).remove();
+                   })
+               });
+       });
+
+
+       $(this).click(function() {
+           copyTextToClipboard(copy($(this).prev(".prettyprint").find("code")));
+           $(this).parent().each(function() {
+               $(this).prepend("<span class='copied-popup'>Copied. </span>");
+               $(this).find('.copy-popup').remove();
+           });
+       });
     });
 }
 
@@ -34,25 +73,29 @@ function copy( codeblock ) {
 
     //credit
 
-    var creditFooter = codeblock.parent().parent().find(".fw");
+    var creditFooter = codeblock.parent().parent().parent().find(".fw");
 
-    var userActionTime = creditFooter.find("div[class=user-action-time]");
-    var userDetails = creditFooter.find("div[class=user-details]");
+    var userActionTime = creditFooter.find("div.user-action-time");
+    var userDetails = creditFooter.find("div.user-details");
 
     var numAuthors = 0;
     userActionTime.each(function( index ) {
-        numAuthors++;
+        numAuthors = numAuthors + 1;
     });
 
-    if(numAuthors == 1) {
+    alert(numAuthors);
 
-        userActionTime = userActionTime.get(0).text().replace(/\s/g,'');
-        userDetails = userDetails.get(0);
+    if(numAuthors <= 1) {
+
+        userActionTime = userActionTime.children(":first").text().replace(/\s/g,'');
+        userDetails = userDetails.children(":first");
+
+        alert("user action time: " + userActionTime + "\nuser details: " + userDetails);
 
         if (userActionTime == 'asked') {
-            text = text + "\n" + commentCharacters + "Code by: " + userDetails.find("a").text();
+            text = text + "\n" + commentCharacters + "Code by: " + userDetails.children("a").text();
         } else if (userActionTime == 'answered') {
-            text = text + "\n" + commentCharacters + "Code by: " + userDetails.find("a").text();
+            text = text + "\n" + commentCharacters + "Code by: " + userDetails.children("a").text();
         }
     } else {
 
@@ -61,9 +104,9 @@ function copy( codeblock ) {
 
         userActionTime.each(function( index ){
            if($(this).text().replace(/\s/g,'')=='answered') {
-             author =  $(this).nextAll("div[class=user-details]").find("a").text();
+             author =  $(this).nextAll("div.user-details").children("a").text();
            } else if($(this).text().replace(/\s/g,'')=='edited') {
-              editor =  $(this).nextAll("div[class=user-details]").find("a").text();
+              editor =  $(this).nextAll("div.user-details").children("a").text();
            }
         });
 
@@ -73,4 +116,45 @@ function copy( codeblock ) {
     }
 
     return text;
+}
+
+function copyTextToClipboard(text) {
+    var copyFrom = $('<textarea/>');
+    copyFrom.text(text);
+    $('body').append(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    copyFrom.remove();
+}
+
+function imgToSVG() {
+    $('img.svg').each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        $.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+
+    });
 }
